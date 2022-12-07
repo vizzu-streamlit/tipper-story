cd4eba1b2e83470d88013ca999709c87cb9cae3a
Peter Vidos <peter@vizzuhq.com>
2022-12-08 00:14:24 +0100

    Update explorer.py

--- explorer.py
+++ explorer.py
@@ -61,7 +61,8 @@ config = {
 }
 
 style = {'plot' : 
-			{'paddingLeft' : '10em', 'xAxis': {'label': {'angle': '2.5'}},
+			{'paddingLeft' : '10em', 
+			'xAxis': { 'label': {'angle': '2.5'}},
 			'yAxis' :{ 'title' :{ 'color' : '#00000000'}},
 			'xAxis' :{ 'title' :{ 'color' : '#00000000'}},
 		#	'marker' :{ 'label' :{ 'position' : 'top'}},
